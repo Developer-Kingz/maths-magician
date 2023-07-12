8a501403954cd7d17a4165bb25ef8a2e61596da6
Kingsley Igbor <chidoziek2@gmail.com>
2023-07-12 13:04:17 +0100

    created a child component and removed boilerplates

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,13 @@
-import React from 'react';
-import '../App.css';
+/* eslint-disable */
+import React from "react";
+import "../App.css";
+import Calculate from "./Calculate.js";
 
 const Calculator = () => (
-  <div className="main">
-    <div className="calculator">
-      <div className="screen">0</div>
-      <div className="input-container">
-        <div className="inputs">AC</div>
-        <div className="inputs">+/-</div>
-        <div className="inputs">%</div>
-        <div className="inputs-right">/</div>
-        <div className="inputs">7</div>
-        <div className="inputs">8</div>
-        <div className="inputs">9</div>
-        <div className="inputs-right">x</div>
-        <div className="inputs">4</div>
-        <div className="inputs">5</div>
-        <div className="inputs">6</div>
-        <div className="inputs-right">-</div>
-        <div className="inputs">1</div>
-        <div className="inputs">2</div>
-        <div className="inputs">3</div>
-        <div className="inputs-right">+</div>
-        <div className="zero-input">0</div>
-        <div className="dot-inputs">.</div>
-        <div className="equal-inputs">=</div>
-      </div>
+    <div className="main">
+        <Calculate />
     </div>
-  </div>
 );
 
 export default Calculator;
+/* eslint-enable */
