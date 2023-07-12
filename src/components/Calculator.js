ee588817c7e16409f536144c865101249b8918b8
Kingsley Igbor <chidoziek2@gmail.com>
2023-07-12 01:32:48 +0100

    designed calculator UI

---
+++ src/components/Calculator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../App.css';
+
+const Calculator = () => (
+  <div className="main">
+    <div className="calculator">
+      <div className="screen">0</div>
+      <div className="input-container">
+        <div className="inputs">AC</div>
+        <div className="inputs">+/-</div>
+        <div className="inputs">%</div>
+        <div className="inputs-right">/</div>
+        <div className="inputs">7</div>
+        <div className="inputs">8</div>
+        <div className="inputs">9</div>
+        <div className="inputs-right">x</div>
+        <div className="inputs">4</div>
+        <div className="inputs">5</div>
+        <div className="inputs">6</div>
+        <div className="inputs-right">-</div>
+        <div className="inputs">1</div>
+        <div className="inputs">2</div>
+        <div className="inputs">3</div>
+        <div className="inputs-right">+</div>
+        <div className="zero-input">0</div>
+        <div className="dot-inputs">.</div>
+        <div className="equal-inputs">=</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Calculator;
