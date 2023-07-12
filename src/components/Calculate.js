413f10c0ce1e1d7a9eea2e4de15aceecd6b7e116
Kingsley Igbor <chidoziek2@gmail.com>
2023-07-12 13:09:44 +0100

    fixed linters

--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -1,36 +1,32 @@
-/* eslint-disable */
-import React from "react";
+import React from 'react';
 
-const Calculate = () => {
-    return (
-        <div>
-            <div className="calculator">
-                <div className="screen">0</div>
-                <div className="input-container">
-                    <div className="inputs">AC</div>
-                    <div className="inputs">+/-</div>
-                    <div className="inputs">%</div>
-                    <div className="inputs-right">/</div>
-                    <div className="inputs">7</div>
-                    <div className="inputs">8</div>
-                    <div className="inputs">9</div>
-                    <div className="inputs-right">x</div>
-                    <div className="inputs">4</div>
-                    <div className="inputs">5</div>
-                    <div className="inputs">6</div>
-                    <div className="inputs-right">-</div>
-                    <div className="inputs">1</div>
-                    <div className="inputs">2</div>
-                    <div className="inputs">3</div>
-                    <div className="inputs-right">+</div>
-                    <div className="zero-input">0</div>
-                    <div className="dot-inputs">.</div>
-                    <div className="equal-inputs">=</div>
-                </div>
-            </div>
-        </div>
-    );
-};
+const Calculate = () => (
+  <div>
+    <div className="calculator">
+      <div className="screen">0</div>
+      <div className="input-container">
+        <div className="inputs">AC</div>
+        <div className="inputs">+/-</div>
+        <div className="inputs">%</div>
+        <div className="inputs-right">/</div>
+        <div className="inputs">7</div>
+        <div className="inputs">8</div>
+        <div className="inputs">9</div>
+        <div className="inputs-right">x</div>
+        <div className="inputs">4</div>
+        <div className="inputs">5</div>
+        <div className="inputs">6</div>
+        <div className="inputs-right">-</div>
+        <div className="inputs">1</div>
+        <div className="inputs">2</div>
+        <div className="inputs">3</div>
+        <div className="inputs-right">+</div>
+        <div className="zero-input">0</div>
+        <div className="dot-inputs">.</div>
+        <div className="equal-inputs">=</div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Calculate;
-/* eslint-enable */
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,11 @@
-/* eslint-disable */
-import React from "react";
-import "../App.css";
-import Calculate from "./Calculate.js";
+import React from 'react';
+import '../App.css';
+import Calculate from './Calculate';
 
 const Calculator = () => (
-    <div className="main">
-        <Calculate />
-    </div>
+  <div className="main">
+    <Calculate />
+  </div>
 );
 
 export default Calculator;
-/* eslint-enable */
