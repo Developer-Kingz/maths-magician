19cb08b8719ef051e9558a7e0778f25c2e1b6685
Kingsley Igbor <chidoziek2@gmail.com>
2023-07-21 04:32:33 +0100

    fixed linters

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import Button from "./Button";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Button from './Button';
+import '@testing-library/jest-dom/extend-expect';
 
-describe("Button", () => {
-    it("renders button text correctly", () => {
-        const buttonText = "Click Me";
-        const { getByText } = render(<Button text={buttonText} />);
-        const buttonElement = getByText(buttonText);
-        expect(buttonElement).toBeInTheDocument();
-    });
+describe('Button', () => {
+  it('renders button text correctly', () => {
+    const buttonText = 'Click Me';
+    const { getByText } = render(<Button text={buttonText} />);
+    const buttonElement = getByText(buttonText);
+    expect(buttonElement).toBeInTheDocument();
+  });
 
-    it("triggers onClick function when clicked", () => {
-        const onClickMock = jest.fn();
-        const { getByText } = render(
-            <Button text="Click Me" onClick={onClickMock} />
-        );
-        const buttonElement = getByText("Click Me");
-        fireEvent.click(buttonElement);
-        expect(onClickMock).toHaveBeenCalled();
-    });
+  it('triggers onClick function when clicked', () => {
+    const onClickMock = jest.fn();
+    const { getByText } = render(
+      <Button text="Click Me" onClick={onClickMock} />,
+    );
+    const buttonElement = getByText('Click Me');
+    fireEvent.click(buttonElement);
+    expect(onClickMock).toHaveBeenCalled();
+  });
 
-    it("applies className correctly", () => {
-        const { container } = render(
-            <Button text="Click Me" className="custom-button" />
-        );
-        const buttonElement = container.querySelector("button");
-        expect(buttonElement).toHaveClass("custom-button");
-    });
+  it('applies className correctly', () => {
+    const { container } = render(
+      <Button text="Click Me" className="custom-button" />,
+    );
+    const buttonElement = container.querySelector('button');
+    expect(buttonElement).toHaveClass('custom-button');
+  });
 
-    // Add more test cases here as needed.
+  // Add more test cases here as needed.
 });
--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Calculator from "./Calculator";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Calculator from './Calculator';
 
-describe("Calculator", () => {
-    it("renders correctly", () => {
-        const { asFragment } = render(
-            <Router>
-                <Calculator />
-            </Router>
-        );
-        expect(asFragment()).toMatchSnapshot();
-    });
+describe('Calculator', () => {
+  it('renders correctly', () => {
+    const { asFragment } = render(
+      <Router>
+        <Calculator />
+      </Router>,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-    // Add more test cases here as needed.
+  // Add more test cases here as needed.
 });
