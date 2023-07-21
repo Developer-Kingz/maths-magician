a34826950f1524f61bc18cbd45dbbf7ed45ac012
rubydevi <ariesabyss@gmail.com>
2023-07-21 13:15:13 +0530

    Created snapshot for Quotes

--- src/components/Quotes.test.js
+++ src/components/Quotes.test.js
@@ -1,32 +1,15 @@
+// Quotes.test.js
 import React from 'react';
-import {
-  render, waitFor, screen, act,
-} from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import fetchMock from 'jest-fetch-mock';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
 import Quotes from './Quotes';
-import '@testing-library/jest-dom/extend-expect';
 
-jest.useFakeTimers();
-
-beforeEach(() => {
-  jest.clearAllMocks();
-  fetchMock.enableMocks();
-});
-
-test('renders error message', async () => {
-  fetchMock.mockRejectOnce(new Error('API Error'));
-
-  render(
+test('Quotes component matches snapshot', () => {
+  // Wrap Quotes component in MemoryRouter
+  const { asFragment } = render(
     <MemoryRouter>
       <Quotes />
     </MemoryRouter>,
   );
-
-  act(() => jest.runAllTimers());
-
-  await waitFor(() => {
-    const errorMessage = screen.queryByText('Cannot load data');
-    expect(errorMessage).toBeInTheDocument();
-  });
+  expect(asFragment()).toMatchSnapshot();
 });
