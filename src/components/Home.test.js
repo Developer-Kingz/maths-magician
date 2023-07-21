7078e88e5a20a0eb5666f247e3ca82123ba7c210
rubydevi <ariesabyss@gmail.com>
2023-07-21 13:14:48 +0530

    Created snapshot for Home

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
 import Home from './Home';
 
-// Mock the react-router useLocation hook
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useLocation: () => ({ pathname: '/' }),
-}));
-
-test('renders welcome message', () => {
-  // Arrange (Render the Home component within the MemoryRouter)
-  const { getByText } = render(
+test('Home component matches snapshot', () => {
+  // Wrap Home component in MemoryRouter
+  const { asFragment } = render(
     <MemoryRouter>
       <Home />
     </MemoryRouter>,
   );
-
-  // Assert (Check if the welcome message is rendered)
-  const welcomeMessage = getByText(/Welcome To Our Page/i);
-  expect(welcomeMessage).toBeInTheDocument();
+  expect(asFragment()).toMatchSnapshot();
 });
