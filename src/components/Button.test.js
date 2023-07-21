afdca90b9b9be8642dcc93a08973872fc8a35901
rubydevi <ariesabyss@gmail.com>
2023-07-21 13:16:19 +0530

    Used Jest snapshot for testing Button component

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Button from './Button';
-import '@testing-library/jest-dom/extend-expect';
 
-describe('Button', () => {
-  it('renders button text correctly', () => {
-    const buttonText = 'Click Me';
-    const { getByText } = render(<Button text={buttonText} />);
-    const buttonElement = getByText(buttonText);
-    expect(buttonElement).toBeInTheDocument();
-  });
-
-  it('triggers onClick function when clicked', () => {
-    const onClickMock = jest.fn();
-    const { getByText } = render(
-      <Button text="Click Me" onClick={onClickMock} />,
-    );
-    const buttonElement = getByText('Click Me');
-    fireEvent.click(buttonElement);
-    expect(onClickMock).toHaveBeenCalled();
-  });
-
-  it('applies className correctly', () => {
-    const { container } = render(
-      <Button text="Click Me" className="custom-button" />,
-    );
-    const buttonElement = container.querySelector('button');
-    expect(buttonElement).toHaveClass('custom-button');
-  });
-
-  // Add more test cases here as needed.
+test('Button component matches snapshot', () => {
+  const { asFragment } = render(
+    <Button text="Click Me" className="custom-btn" onClick={() => {}} />,
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
